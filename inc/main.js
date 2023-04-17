3b89bfd599ecdfd59ea3c62d2ac18c49384ceee2
Lene <lenecarpertunnel@gmail.com>
2023-04-17 11:29:30 +0200

    Dynamically add images

--- inc/main.js
+++ inc/main.js
@@ -5,18 +5,41 @@ let words = ['A double-handed sword', 'A medieval set of armor', 'A large castle
 let images = ['sword.png', 'armor.png', 'castle.png', 'bow.png', 'catapult.png'];
 
 // Declare variables
-// $imageContainer = document.getElementById('image-container');
+$checkedCounter = 0;
 
 function init()
 {
-    for (let i = 0; i < words.length; i++)
+    for (let i = 0; i < images.length; i++)
     {
         var img = document.createElement("img");
         img.src = "img/" + images[i];
         var src = document.getElementById("image-container");
         src.appendChild(img);
     }
+    
+    for (let i = 0; i < words.length; i++)
+    {
+        var para = document.createElement("p");
+        para.innerHTML = words[i];
+        document.getElementById("word-container").appendChild(para) + "<br>";
+    }
 }
+
+$(document).ready(function() {
+    $("#image-container img").on("click", function() {
+        console.log($(this));
+        $(this).css('border-color', 'red');
+        $checkedCounter++;
+        console.log($checkedCounter);
+
+        if ($checkedCounter == 2)
+        {
+            $checkedCounter = 0;
+            $("#image-container img").css('border-color', 'gray');
+            console.log('alert!!!!');
+        }
+    })
+})
 
 
 init();
--- index.php
+++ index.php
@@ -8,7 +8,13 @@
     <link rel="stylesheet" href="style/style.css" />
 </head>
 <body>
-    <div id="image-container"></div>
+    <div id="flex-container">
+        <div id="game-container">
+            <div id="word-container"></div>
+            <div id="image-container"></div>
+        </div>
+    </div>
+
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js'></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="inc/main.js"></script>
